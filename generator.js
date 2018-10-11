5ceb17fd0604a83475fff195a2b189ff8ae32ec7
tnburt <travisnburt@gmail.com>
2018-10-10 20:57:00 -0600

    Fullscreen again, options working, adjust canvas size

--- generator.js
+++ generator.js
@@ -1,55 +1,81 @@
-var canvasWidth = document.getElementById('landscape').getAttribute('width');
-var canvasHeight = document.getElementById('landscape').getAttribute('height');
-
-var variationValue = document.getElementById('variation').value;
-var intensity = document.getElementById('intensity').value;
-var waterLevel = document.getElementById('waterLevel').value;
-
-var randomness = Math.floor(canvasWidth / variationValue);
+const canvasWidth = document.getElementById('landscape').getAttribute('width');
+const canvasHeight = document.getElementById('landscape').getAttribute('height');
 
 var terrain = new Array();
 
-var chunk = new Array(randomness);
-for(i=0; i<variationValue; i++){
-    chunk.fill(0);
-    addChunk();
+create();
+
+function procGen() {
+    clear();
+    create();
 }
 
-draw();
+function create() {
+    let variationValue = document.getElementById('variation').value;
+    let intensity = document.getElementById('intensity').value;
+    
+    for(i=0; i<variationValue; i++){
+        addChunk(intensity, variationValue);
+    }
 
-function addChunk(){
-    var startPoint = Math.random()*intensity + ((canvasHeight/2) - (intensity/2));
+    draw();
+}
+
+function clear() {
+    terrain = [];
+}
+
+function addChunk(intensity, variationValue){
+    let chunkSize = Math.floor(canvasWidth / variationValue);
+    let chunk = new Array(chunkSize);
+    chunk.fill(0);
+
+    let startPoint = Math.random()*intensity + ((canvasHeight/2) - (intensity/2));
     if(terrain.length > 0) {
         startPoint = terrain[terrain.length-1]
     }
     chunk[0] = Math.round(startPoint);
-    chunk[randomness] = Math.round(Math.random()*intensity) + ((canvasHeight/2) - (intensity/2));
+    chunk[chunkSize] = Math.round(Math.random()*intensity) + ((canvasHeight/2) - (intensity/2));
 
-    generate(0, randomness);
+    generate(0, chunkSize, chunk);
     terrain = terrain.concat(chunk);
 }
 
-function generate(minPoint, maxPoint){
-    var middlePoint = Math.round((minPoint+maxPoint)/2);
-    var pointBetween = Math.round((chunk[minPoint]+chunk[maxPoint])/2);
-    var pointRange = Math.abs(chunk[maxPoint]-chunk[minPoint]);
+function generate(minPoint, maxPoint, chunk){
+    let middlePoint = Math.round((minPoint+maxPoint)/2);
+    let pointBetween = Math.round((chunk[minPoint]+chunk[maxPoint])/2);
+    let pointRange = Math.abs(chunk[maxPoint]-chunk[minPoint]);
 
     if(minPoint == middlePoint) return;
     if(maxPoint == middlePoint) return;
 
-    var deviation = Math.round(Math.random()*pointRange);
+    let deviation = Math.round(Math.random()*pointRange);
     chunk[middlePoint] = Math.round(pointBetween + (deviation - (pointRange/2)));
 
-    generate(minPoint, middlePoint);
-    generate(middlePoint, maxPoint);
+    generate(minPoint, middlePoint, chunk);
+    generate(middlePoint, maxPoint, chunk);
 }
 
 function draw(){
-    var landscape = document.getElementById('landscape');
-    var ctx = landscape.getContext("2d");
+    let waterLevel = document.getElementById('waterLevel').getAttribute('max') - document.getElementById('waterLevel').value;
+
+    let landscape = document.getElementById('landscape');
+    let ctx = landscape.getContext("2d");
+    ctx.clearRect(0,0,landscape.width, landscape.height);
+    
+    let water = document.getElementById('water');
+    let waterCtx = water.getContext("2d");
+    waterCtx.clearRect(0,0,landscape.width, landscape.height);
+
+    let waterDepth = waterCtx.createLinearGradient(0,waterLevel,0,waterLevel+200);
+    waterDepth.addColorStop(0, "teal");
+    waterDepth.addColorStop(1, "black");
+    waterCtx.fillStyle = waterDepth;
+    waterCtx.fillRect(0, waterLevel, canvasWidth, canvasHeight);
 
     xIncrement = landscape.width / (terrain.length-1);
-    for(var x=0; x<=terrain.length; x++){
+
+    for(let x=0; x<=terrain.length; x++){
 
         if(terrain[x] < waterLevel){
             ctx.beginPath();
@@ -65,23 +91,14 @@ function draw(){
             ctx.stroke();
         } else {
             ctx.beginPath();
-            ctx.moveTo(x, waterLevel);
-            ctx.lineTo(x, terrain[x]+0);
-            ctx.strokeStyle="teal";
-            ctx.stroke();
-
-            ctx.beginPath();
             ctx.moveTo(x, terrain[x]+0);
             ctx.lineTo(x, terrain[x]+20);
             ctx.strokeStyle="tan";
             ctx.stroke();
         }
 
-        ctx.beginPath();
-        ctx.moveTo(x, terrain[x]+20);
-        ctx.lineTo(x, 500);
-        ctx.strokeStyle="grey";
-        ctx.stroke();
+        ctx.fillStyle="grey";
+        ctx.fillRect(x, terrain[x], 1, canvasHeight-terrain[x]);
     }
 }
 
@@ -93,8 +110,4 @@ function updateSliderIntensity(value){
 }
 function updateSliderWater(value){
     document.querySelector('#numWater').value = value;
-}
-
-function procGen() {
-    
 }
